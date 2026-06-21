0de34e5b960f5ce24b15099dfbdbb20716aa3595
Abhishek <singh752007abhishek@gmail.com>
2026-06-21 22:41:32 +0530

    Invariant test protocallMustHaveMoreValueThenTotalSupply added

---
+++ test/fuzz/OpenInvariantsTests.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+// Have our invariant aka properties
+// What are our invariants?
+// 1. The total supply of DSC should be less than the total value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
+    address weth;
+    address wbtc;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+
+        targetContract(address(dsce));
+    }
+
+    function invariant_protocallMustHaveMoreValueThenTotalSupply()public view {
+        //get value of all the collateral in protocall
+        //compare it to all the debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
